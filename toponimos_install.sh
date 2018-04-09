ed5c075ed67efead7d235d56779904884c169540
tepege <38130347+tepege@users.noreply.github.com>
2018-04-09 12:24:17 +0200

    Update toponimos_install.sh

--- toponimos_install.sh
+++ toponimos_install.sh
@@ -5,4 +5,4 @@ sudo git clone https://github.com/OCA/l10n-spain.git -b 10.0 /opt/odoo/l10n-spai
 
 #2.- Crea los accesos directos
 echo -e "\nCreando enlaces..."
-sudo ln -s /opt/odoo/vertical-ngo/l10n_es_toponyms /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/l10n-spain/l10n_es_toponyms /opt/odoo/custom/addons
