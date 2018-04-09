a47bd47b2668bee587c36b9bb4e38914571ef5ab
tepege <38130347+tepege@users.noreply.github.com>
2018-04-09 12:48:11 +0200

    Update toponimos_install.sh

--- toponimos_install.sh
+++ toponimos_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #1.- Descarga
 echo -e "\nDescargando archivo..."
-sudo git clone https://github.com/OCA/l10n-spain.git -b 10.0 /opt/odoo/partner-contact
-sudo git clone https://github.com/OCA/partner-contact.git -b 10.0 /opt/odoo/l10n-spain
+sudo git clone https://github.com/OCA/l10n-spain.git -b 10.0 /opt/odoo/l10n-spain
+sudo git clone https://github.com/OCA/partner-contact.git -b 10.0 /opt/odoo/partner-contact
 
 #2.- Crea los accesos directos
 echo -e "\nCreando enlaces..."
